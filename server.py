1cbc7d6065c2e244b9fdd857094090b002efd246
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-01-27 21:59:45 +0530

    Added All The Features

--- server.py
+++ server.py
@@ -30,7 +30,34 @@ def aakarsh():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.html",tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_delete = Todo.query.get_or_404(id)
 
+    try:
+        db.session.delete(task_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There is issue in deleting task'
+    
+
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method =='POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There is issue in update task'
+    else:
+            return render_template('update.html',task=task)
+ 
     
 
 with app.app_context():
